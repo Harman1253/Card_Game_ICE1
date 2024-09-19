d6db3bdf24491981c72b1afa2d0577a00c35a455
Harman1253 <sin14332@sheridancollege.ca>
2024-09-18 22:20:32 -0400

    Card Game version 1

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -10,23 +10,48 @@ package card;
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author srinivsi
  */
+import java.util.Random;
+import java.util.Scanner;
+
 public class CardTrick {
-    
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         Card[] magicHand = new Card[7];
-        
-        for (int i=0; i<magicHand.length; i++)
-        {
+        Random random = new Random();
+
+        // Fill magicHand with random card objects
+        for (int i = 0; i < magicHand.length; i++) {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue(random.nextInt(13) + 1); // random value between 1 and 13 (Ace to King)
+            c.setSuit(Card.SUITS[random.nextInt(4)]); // random suit (Hearts, Diamonds, Spades, Clubs)
+            magicHand[i] = c;
         }
-        
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
-        // add one luckcard hard code 2,clubs
+
+        // Ask the user for card value and suit
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Pick a card value (1 for Ace, 11 for Jack, 12 for Queen, 13 for King): ");
+        int userValue = scanner.nextInt();
+
+        System.out.println("Pick a suit (0 for Hearts, 1 for Diamonds, 2 for Spades, 3 for Clubs): ");
+        int userSuitIndex = scanner.nextInt();
+
+        String userSuit = Card.SUITS[userSuitIndex];
+
+        // Search the magicHand for the user's card
+        boolean cardFound = false;
+        for (Card card : magicHand) {
+            if (card.getValue() == userValue && card.getSuit().equals(userSuit)) {
+                cardFound = true;
+                break;
+            }
+        }
+
+        // Report the result
+        if (cardFound) {
+            System.out.println("Your card was found in the magic hand!");
+        } else {
+            System.out.println("Your card was not found in the magic hand.");
+        }
     }
-    
 }
